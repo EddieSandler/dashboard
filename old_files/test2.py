31fb2afd3e0ff6899fb5804c44f6198253109f49
Eddie Sandler <ed.sandler@gmail.com>
2024-10-17 19:13:11 -0400

    refactoring

--- old_files/test2.py
+++
@@ -1,6 +0,0 @@
-from yahooquery import Ticker
-
-ticker = Ticker('AAPL')
-latest_news = ticker.news()
-
-print(latest_news)
--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ getEcoNums();
 get_econ_calendar();
 
 let watchlistButtonClickListener;
-BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = 'http://127.0.0.1:5000';
 
 
 let userId = document.getElementById('userId');
